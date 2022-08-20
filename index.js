ea72d1a5c10ded9098a55efc11b2c9fea3d1f0d6
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-20 18:20:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Plyer 1 Wins!!";
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!";
 }
 else if(randomNumber2 > randomNumber1){
     document.querySelector("h1").innerHTML = "Player 2 Wins";
